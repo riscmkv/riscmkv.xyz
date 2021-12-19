ee617029772284f54ad839822f86f14a381e6a48
TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>
2021-12-19 09:17:13 -0600

    Fixed usually unsupported one liner closing tag

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
         gtag('config', 'G-2S0Z8XV28Y');
     </script>
 
-    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
 </head>
 
 <body>
